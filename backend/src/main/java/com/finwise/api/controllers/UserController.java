5d87f2d206c7419bf359e9eab7d2249525a38769
harshalwasnik22 <hwasnik22@gmail.com>
2025-03-24 09:07:04 +0000

    added user controller

---
+++ backend/src/main/java/com/finwise/api/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.finwise.api.controllers;
+
+import com.finwise.api.models.User;
+import com.finwise.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
